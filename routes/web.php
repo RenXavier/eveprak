fa446cc63915beed7b53b6d71897c66fc2744549
Maruko88 <paduam451@gmail.com>
2025-05-26 00:37:29 +0800

    try mo na

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,13 @@ Route::middleware('auth')->group(function () {
     
     Route::get('employee', [App\Http\Controllers\employeecontroller::class, 'index']);
     Route::post('employee', [App\Http\Controllers\employeecontroller::class, 'index']);
-    
-    Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
-    Route::get('employee', [\App\Http\Controllers\employeecontroller::class, 'index'])->name('employee.index');
-    Route::get('employee/create', [App\Http\Controllers\employeecontroller::class, 'create'])->name('employee.create');
-    Route::post('employee', [App\Http\Controllers\employeecontroller::class, 'store'])->name('employee.store');
-    Route::get('employee/{id}/edit', [App\Http\Controllers\employeecontroller::class, 'edit'])->name('employee.edit');
-    Route::put('employee/{id}/edit', [App\Http\Controllers\employeecontroller::class, 'update'])->name('employee.edit');
-    Route::get('employee/{id}/delete', [App\Http\Controllers\employeecontroller::class, 'delete'])->name('employee.delete');
+    Route::get('users', [\App\Http\Controllers\UserController::class, 'index']->name('users.index');
+    Route::get('employee', [\App\Http\Controllers\employeecontroller::class, 'index']->name('employee.index'));
+    Route::get('employee/create', [App\Http\Controllers\employeecontroller::class, 'create']->name('employee.create'));
+    Route::post('employee', [App\Http\Controllers\employeecontroller::class, 'store']->name('employee.store'));
+    Route::get('employee/{id}/edit', [App\Http\Controllers\employeecontroller::class, 'edit']->name('employee.edit'));
+    Route::put('employee/{id}/edit', [App\Http\Controllers\employeecontroller::class, 'update']->name('employee.edit'));
+    Route::get('employee/{id}/delete', [App\Http\Controllers\employeecontroller::class, 'delete']->name('employee.delete'));
 
     
 
